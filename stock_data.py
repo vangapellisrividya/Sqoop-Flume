85fc56dcd615f8ac1d84000cbc39dafcd8f8fed8
vangapellisrividya <87218796+vangapellisrividya@users.noreply.github.com>
2022-01-18 10:01:38 +0530

    python code to ingest stockdata to hdfs

---
+++ stock_data.py
@@ -0,0 +1,11 @@
+import csv
+import requests
+# replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
+CSV_URL = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY_EXTENDED&symbol=IBM&interval=15min&slice=year1month1&apikey=4X0A1IGHNXUX93SO'
+with requests.Session() as s:
+    download = s.get(CSV_URL)
+    decoded_content = download.content.decode('utf-8')
+    cr = csv.reader(decoded_content.splitlines(), delimiter=',')
+    my_list = list(cr)
+    for row in my_list:
+        print(row)
